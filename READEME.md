a71e19f14a4af422ae35fe8151130f7dfd12ee0a
Colin Ferris <colinferris95@gmail.com>
2018-07-27 16:40:32 +0000

    change api to asynch and readme update

---
+++ READEME.md
@@ -0,0 +1,9 @@
+Problem: build an API that queries mutilpe HTTP endpoints and return a merged list conatining all of the results sorted by agony
+
+To solve this problem, I decide to stick with the technology used in the searchrunner api, the tornado webserver
+I needed to gather all of the resources and sort them by a specific key. 
+The first implementation I did used the urllib library. This provide the correct answer, but the HTTP calls were too slow,
+So I switched to the torando.httpclient, AsyncHTTPClient. This allowed me to yield multiple endpoints in an asynchronous fashion. 
+This made the api much faster.
+
+To run the server, use the same command as the searchrunner api, python -m flightsearchapi
--- flightsearchapi.py
+++ flightsearchapi.py
@@ -1,81 +1,98 @@
-#Problem: Your job is to build an API that queries each provider via HTTP and returns a merged list containing all of their results.
+#Problem: Your job is to build an API that queries each provider via HTTP and returns a merged list containing all of their results. (sorted by agony)
 
+#use for handling json
+import json, operator
 
+#tornado webserver
 from tornado import gen, ioloop, web
-import urllib, json, operator
+
+#async client
+from tornado.httpclient import AsyncHTTPClient
+
+#also for handling json
+from tornado.escape import json_decode, json_encode
+
 
 class MainHandler(web.RequestHandler):
+    #async keywords
+    @gen.coroutine
     def get(self):
         
-        #get the resource for each endpoint
-        url = "http://45.55.150.116:9000/scrapers/expedia"
-        response = urllib.urlopen(url)
-        data_expedia = json.load(response)
+        #get all the fetchs from the aync call function
+        f_exp = async_calls("http://localhost:9000/scrapers/expedia")
+        f_tra = async_calls("http://localhost:9000/scrapers/travelocity")
+        f_orb = async_calls("http://localhost:9000/scrapers/orbitz")
+        f_pri = async_calls("http://localhost:9000/scrapers/priceline")
+        f_uni = async_calls("http://localhost:9000/scrapers/united")
         
-        url = "http://45.55.150.116:9000/scrapers/travelocity"
-        response = urllib.urlopen(url)
-        data_travelocity = json.load(response)
-        
-        url = "http://45.55.150.116:9000/scrapers/orbitz"
-        response = urllib.urlopen(url)
-        data_orbitz = json.load(response)
-        
-        url = "http://45.55.150.116:9000/scrapers/priceline"
-        response = urllib.urlopen(url)
-        data_priceline = json.load(response)
-        
-        url = "http://45.55.150.116:9000/scrapers/united"
-        response = urllib.urlopen(url)
-        data_united = json.load(response)
-       
-        #just get the data without the results[] array
-        json_string_expedia = data_expedia["results"]
-   
-        json_string_travelocity = data_travelocity["results"]
-        
-        json_string_orbitz = data_orbitz["results"]
-        
-        json_string_priceline = data_priceline["results"]
-        
-        json_string_united = data_united["results"]
-        
-        print(len(json_string_united) )
-        
-        print(json_string_united[0]["agony"])
+    
+        #yeild the async fetches
+        f_exp = yield f_exp
+        f_tra = yield f_tra
+        f_orb = yield f_orb
+        f_pri = yield f_pri
+        f_uni = yield f_uni
         
         
+        #format our json resources
+        data_expedia = format_json_resource(f_exp.body)
+        data_travelocity = format_json_resource(f_tra.body)
+        data_orbitz = format_json_resource(f_orb.body)
+        data_priceline = format_json_resource(f_pri.body)
+        data_united =format_json_resource(f_uni.body)
         
         
+        #make one big merged list
+        data = { 'results' : data_expedia + data_travelocity + data_orbitz + data_priceline + data_united }
         
-        #merge arrays list(heapq.merge(json_string_expedia,json_string_travelocity,json_string_orbitz,json_string_priceline,json_string_united))
-        
-        
-        #add all of the json together
-        data = { 'results' : json_string_expedia + json_string_travelocity + json_string_orbitz + json_string_priceline + json_string_united }
-        
-        #sort by key 'agony'
+        # sort by key 'agony'
         data["results"].sort(key=operator.itemgetter('agony'))
         
-        #write json to page
+        
+        #get data into a format that can write to browser
         data = json.dumps(data)
 
         self.write(data)
+        self.finish()
+
+
+#aysnc fetch function to get endpoints        
+def async_calls(url):
+
+    #start up our aysnc client
+    http_client = AsyncHTTPClient()
+    
+    #fetch the endpoint    
+    response =  http_client.fetch(url)
+        
+   
+    return response
   
-
- 
-
+  
+#json formatting function to prepare json for merging    
+def format_json_resource(json_resource):
+    json_raw = json_decode(json_resource)
+    
+    json_non_array = json_raw["results"]
+    
+    return json_non_array
+    
+    
 #use correct endpoint
 ROUTES = [
     (r"/flights/search", MainHandler),
 ] 
 
-#
+#main function
 def run():    
     app = web.Application(
         ROUTES,
         debug=True,
     )
-    app.listen(8000, address='45.55.150.116')
+    app.listen(8000)
+    
+    print "Server (re)started. Listening on port 8000"
+    
     ioloop.IOLoop.current().start()
 
 
